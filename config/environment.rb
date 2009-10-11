9a533ebfb8f398abb371af7262bc81f40aa01250
Eric Hutzelman <ehutzelman@gmail.com>
2009-10-11 17:52:29 -0500

    gem dependencies and initializers

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  # config.gem 'chrislloyd-gravtastic', :lib => 'gravtastic', :source => 'http://gems.github.com', :version => '>= 2.0.0'
+  config.gem 'ruby-openid', :lib => false, :version => '>= 2.1.7'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
