14e052903cc9989b92c87011818eb52c52c7e651
Eric Hutzelman <eric@hutzelman.com>
2009-10-12 02:23:39 -0500

    adding in formtastic gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'chrislloyd-gravtastic', :lib => 'gravtastic', :source => 'http://gems.github.com', :version => '>= 2.0.0'
   config.gem 'ruby-openid', :lib => false, :version => '>= 2.1.7'
+  config.gem "justinfrench-formtastic", :lib => 'formtastic', :source => 'http://gems.github.com'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
