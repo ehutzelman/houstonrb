8235a8723d22838c87317048914b57a8ef6f5f34
Eric Hutzelman <ehutzelman@gmail.com>
2009-10-23 00:00:35 -0500

    google map link

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,11 +1,17 @@
 module MeetingsHelper
   
-  def format_address(address)
+  def format_address_for_display(address)
     formatted = ''
     address.split(',', 3).each do |line|
       formatted << "#{line}<br/>"
     end
     formatted
+  end
+
+  def format_address_for_map_url(address)
+    formatted = address.clone
+    formatted.slice!(0..formatted.index(','))
+    CGI::escape(formatted)
   end
   
 end
