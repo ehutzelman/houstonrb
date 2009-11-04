8d987a2be6628ff9eda0369b344b6ebd9e9e3fb2
Eric Hutzelman <eric@hutzelman.com>
2009-11-03 23:46:55 -0600

    setup for heroku env config loader

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,10 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+# Load heroku vars from local file
+heroku_env = File.join(RAILS_ROOT, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
