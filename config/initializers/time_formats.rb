31699d51f81a19022e78f504351c9ca050e6dd69
Eric Hutzelman <ehutzelman@gmail.com>
2009-11-10 19:52:33 -0600

    use named date/time formats for meeting display

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,7 +2,9 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :us => '%m/%d/%Y',
   :us_with_time => '%m/%d/%y, %l:%M %p',
   :short_day => '%e %B %Y',
+  :medium_day => "%A, %B %d",
   :long_day => '%A, %e %B %Y',
   :short_date => "%x",
-  :long_date => "%B %d, %Y"
+  :long_date => "%B %d, %Y",
+  :time => "%I:%M %p"
 )
