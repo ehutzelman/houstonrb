adca5e7b8431e35b2355afa2d7324db251d92711
Eric Hutzelman <ehutzelman@gmail.com>
2010-08-11 15:42:19 -0500

    rescue exception when converting post, upgrade to rails 2.3.8

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,20 +4,20 @@ class Post
   attr_accessor :title, :author, :description, :url, :created_at
 
   RSS_URL = "http://rss.groups.yahoo.com/group/Houston-RoR/rss"
-  
+
   def self.all
     posts = []
     REXML::Document.new(messages_xml).elements.to_a("//item").each do |element|
       posts << create_post_from_xml(element)
     end
-    
+
     posts
   end
 
   def self.messages_xml
     URI.parse(RSS_URL).read
   end
-  
+
   def self.create_post_from_xml(xml)
     post = Post.new
     post.title = xml.elements['title'].text
@@ -25,8 +25,9 @@ class Post
     post.created_at = Time.parse(xml.elements['pubDate'].text)
     post.author = xml.elements['dc:creator'].text
     post.url = xml.elements['link'].text
-
+    post
+  rescue:
     post
   end
-  
+
 end
