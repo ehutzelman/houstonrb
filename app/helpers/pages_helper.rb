678a972ab4d8d9ef755a49ceaf56be6da0e0b6e5
Eric Hutzelman <ehutzelman@gmail.com>
2011-03-01 16:25:42 -0600

    strip spaces on openid url, cleanup campfire link

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+
+  def campfire_link
+    link_to_if(current_user, 'Houston.rb campfire chat room',
+               'http://zipzoomauto.campfirenow.com/1c8f5') do |name|
+      link_to(name, login_path)
+    end
+  end
+
 end
